7c8b90cd8ed69a3afc7116b7625791e5ab92b501
GuysRGithub <guysdeveloperrobot@gmail.com>
2024-03-13 21:23:45 +0700

    Setup simple 3d model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import Loader from '../components/Loader'
+import { Island } from '../models/Island'
+import { ScrollControls } from '@react-three/drei'
 
 const Home = () => {
+  const adjuctIslandForScreen = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [.1, .47, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenPosition, screenScale, rotation]
+  }
+
+  const [screenPosition, screenScale, rotation] = adjuctIslandForScreen();
+
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas className='w-full h-screen bg-transparent'
+        camera={{ near: 0.1, far: 1000 }}>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island position={screenPosition} scale={screenScale} rotation={rotation} />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
